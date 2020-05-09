d465afa58b0927446377bb0620c827b9309b729a
ayush-csulb <ayush.vij@student.csulb.edu>
2020-05-09 11:24:19 -0700

    Step 1 - Added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
   title = 'Final Exam - IS645';
   model: any = {};
   onSubmit() {
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model, null, 4));
+    
+    // alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model, null, 4));
   }
 }
